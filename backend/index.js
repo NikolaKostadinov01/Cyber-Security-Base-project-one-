fe56aceee58440121ba69a417fe018bdb5c93114
NikolaKostadinov01 <105849147+NikolaKostadinov01@users.noreply.github.com>
2024-05-28 00:50:08 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const errorHandler = require('./middlewares/errorHandler');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use('/auth', authRoutes);
+app.use('/tasks', taskRoutes);
+
+// Error handling middleware
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
